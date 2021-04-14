0fbdc68acd2ccb90c6a31e5a7771234bf4bb258c
evelyn9521 <evelyn9521@outlook.es>
2021-04-14 19:19:24 +0200

    Repaso

--- JAVASCRIPT/Ejercicios/Ejercicio_3/main.js
+++ JAVASCRIPT/Ejercicios/Ejercicio_3/main.js
@@ -1,100 +1,66 @@
-//APARTADO 1
-let = employees = []
-for (let i = 1; i <= 100; i++){
+//1...GENERAR UN ARRAY DE 100 OBJETOS CON LAS SIGUIENTES PROPIEDADES:
+let employees = [];
+for (let i = 0; i <= 100; i++){
     const newEmployee = {
-        cargo:`Empleado ${i}`,
-        rendimiento: Number(Math.random().toFixed(2)), //Number puede sustituirse por parsefloat
-        salario:parseInt(Math.random()* (4000 - 1250) + 1250)
-    };
-    employees.push(newEmployee);
-}
-console.log(employees);
-
-//APARTADO 1 CON CONSTRUCTOR
-// function Employee(cargo, rendimiento, salario){
-//     this.cargo = cargo;
-//     this.rendimiento = rendimiento;
-//     this.salario = salario;
-// }
-// for (let i = 1; i <= 100; i++){
-//     employees.push(new Employee(
-//         `Empleado${i}`, 
-//         Number(Math.random().toFixed(2)), 
-//         parseInt(Math.random()* (4000 - 1250) + 1250)));
-// };
-
-
-//APARTADO 2, Ordenar el array por rendimiento e imprimirlo. Usar una función anónima como callback.
-const ordenarRendimiento = employees.sort((empleado1, empleado2) => empleado1.rendimiento - empleado2.rendimiento);
-console.log(ordenarRendimiento);
-
-//Otra manera de hacerlo con funcion anónima
-// employees.sort(function(empleado1, empleado2){
-//     return empleado1.rendimiento - empleado2.rendimiento
-// });
-//console.log(employees);
-
-
-//APARTADO 3, Ordenar el array por salario e imprimirlo.  Usar una función de flecha.
-const ordenarSalario = employees.sort((empleado1, empleado2) =>empleado1.salario-empleado2.salario);
-console.log(ordenarSalario);
-
-
-//APARTADO 4, Ordenar el array por el número de empleado de forma decreciente. El número de empleado sólo estará dentrodel stringCargo. Usar una función nombrada como callback
-function decreciente(empleado1, empleado2){
-return empleado2.cargo.split(" ")[1] - empleado1.cargo.split(" ")[1]; //el corchete con el 1 se pone para que te ordene el numero
-}
-employees.sort(decreciente);
-console.log(employees);
-
-
-//APARTADO 5, Usando filter: imprimir el cargo y salario de los que cobren más de 2500€
-employees
-.filter(employee => employee.salario >2500)
-.forEach(employee => console.log(employee.cargo, employee.salario));
-
-
-//APARTADO 6, Usando map: subir el sueldo un 25% a los que cobren menos de 1500€ y volver a hacer el punto 5.
-employees = employees.map(employee => {
-    if (employee.salario < 1500) {
-        // Modificar el salario
-        employee.salario = employee.salario * 1.25;
+        position: `Employee ${i}`,
+        performance: Number(Math.floor(Math.random()*100)),
+        salary:parseInt(Math.random() * (4000 - 1250) + 1250)
     }
-    return employee;
-});
-console.log(employees);
+    employees.push(newEmployee); //Relleno el array vacio, con el newEmployee y sus propiedades.
+}
+
+//2...ORDENAR EL ARRAY POR RENDIMIENTO (PERFORMANCE) E IMPRIMIRLO.
+const orderPerformance = employees.sort((employee1, employee2)=> employee1.performance - employee2.performance);
+console.log(orderPerformance); 
 
 
-//APARTADO 7, Usando reduce: Obtener el coste total de todos los sueldos
-// para la empresa teniendo en cuenta que a la empresa le cuesta tener un empleado su sueldo más un 15% por impuestos
-
-const totalCost = employees.reduce((salarySum, employee) =>salarySum + employee.salario *1,15, 0); 
-console.log(totalCost);
+//3...ORDENAR EL ARRAY POR SALARIO (SALARY) E IMPRIMIRLO.
+const orderSalary = employees.sort((employee1, employee2) =>employee1.salary - employee2.salary);
+console.log(orderSalary);
 
 
-//APARTADO 8, 
-//Despedir a los que tengan un rendimiento menor a 0.3.
-employees = employees.filter(employee => employee.rendimiento >= 0.3);
-console.log("despedir a los que tengan ",employees);
+//4...ORDENAR EL ARRAY POR EL Nº DE EMPLEADO DE FORMA DECRECIENTE. EL Nº DE EMPLEADO
+//SOLO ESTARÁ DENTRO DEL STRING CARGO (POSITION).
+ const orderNumEmployee = employees.sort((employee1, employee2) =>employee2.position.split(" ")[1] - employee1.position.split(" ")[1]);
+ console.log(orderNumEmployee);//el [1] se añade para que te ordene desde el primero al ultimo
+                                //el split serapa la palabra empleado del nº con el que vamos a necesitar
 
 
+ //5...USANDO FILTER IMPRIMIR EL CARGO Y SALARIO DE LOS QUE COBREN MÁS DE 2500€
+ employees
+ .filter(employee => employee.salary >2500)
+ .forEach(employee => console.log(employee.position, employee.salary));
 
-//Calcular el sueldo medio de la empresa.
-const averagesSalary = employees.reduce((salarySum, employee) => salarySum + employee.salario, 0) / employees.length;
-console.log("Sueldo medio de la empresa", averagesSalary);
 
-//Subir el sueldo de los que tengan un rendimiento superior a 0.7.
-// employees = employees.map(employee => {
-//     if (employee.rendimiento < 0.7) {
-//         // Modificar el salario
-//         employee.salario = employee.salario * 1.25;
-//     }
-//     return employee;
-// });
+ //6...USANDO MAP, SUBIR EL SUELDO UN 25% A LOS QUE COBREN MENOS DE 1500€ Y VOLVER A HACER EL PUNTO 5
+const incrementSalary = employees.map(employee =>{
+    if(employee.salary < 1500){
+    employee.salary = employee.salary * 1.25
+    }
+    return employee;//devuelve todos los empleados y los empleados con el sueldo subido al 25%
+})
+console.log(incrementSalary);
 
-employees.forEach(employee =>{
+
+//7...USANDO REDUCE, OBTENER EL COSTE TOTAL DE TODOS LOS SUELDOS, TENIENDO EN CUENTA QUE A LA EMPRESA
+//LE CUESTA TENER A UN EMPLEADO SU SUELDO MÁS UN 15% DE IMPUESTOS.
+
+const totalCost = employees.reduce((sum, employee) =>sum + employee.salary *1,15, 0 ); 
+console.log('La suma total de los sueldos es de ', totalCost);
+
+
+//8...DESPEDIR A LOS QUE TENGAN UN RENDIMIENTO MENOS A 0.3
+ const out = employees.filter(employee => employee.performance >= 0.3);
+ console.log('despedir a os que tengan', out);
+
+ //CALCULAR EL SUELDO MEDIO DE LA EMPRESA
+ const averagesSalary = employees.reduce((salarySum, employee) => salarySum + employee.salary, 0) / employees.length;
+ console.log("Sueldo medio de la empresa", averagesSalary);
+ 
+ //SUBIR EL SUELDO DE LOS QUE TENGAN UN RENDIMIENTO SUPPERIOR A 0.7
+ employees.forEach(employee =>{
     if(employee.rendimiento > 0.7){
-        employee.salario = employee.salario *1.25
+        employee.salary = employee.salary *1.25
     }
 })
 console.log("subir sueldo a los que tengan un rendimiento mayor a 0.7", employees);
