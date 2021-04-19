d55d3a72d1ca1b4c03ba8e0f47ae0f289cf1fa92
Evelyn <evelyn9521@outlook.es>
2021-04-19 13:56:13 +0200

    React

---
+++ REACT/CheatSheet/my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import FirstComponents from './components/FirstComponents';
+
+function App() {
+  return (
+    <div className="App">
+     <FirstComponents/>
+     <FirstComponents/>
+     <FirstComponents/>
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT/CheatSheet/my-app/src/components/FirstComponents.js
@@ -0,0 +1,10 @@
+export default function FirstComponents() {
+    return (
+        <div>
+            <h3>Título</h3>
+            <small>16 de Agosto de 2025</small>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis doloribus quo mollitia officia, totam perspiciatis esse quae quas necessitatibus tenetur qui ipsum hic accusantium.</p>
+        </div>
+    )
+}
+//export default FirstComponents// alternativa
